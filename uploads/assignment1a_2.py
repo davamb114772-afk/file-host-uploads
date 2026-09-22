65ce191ed5dbd8af3a99c4c5b37072f2e279c5a1
davamb114772-afk <davamb114772@gmail.com>
2026-09-22 17:41:51 -0300

    Delete assignment1a.py

--- uploads/assignment1a_2.py
+++
@@ -1,5 +0,0 @@
-# assignment1a.py
-# Greeting program - demonstrates input and f-string output
-name = input('What is your name? ')
-print(f'Hello, {name}! Welcome to CS 110 Programming.')
-print('This program shows how we can make the computer talk to us.')
